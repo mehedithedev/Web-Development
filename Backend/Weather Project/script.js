d4c5d73a30627a7a3a3f0e9df0cd003fb75cc38c
Mehedi Hasan <mehedihasan2882@icloud.com>
2023-01-05 23:29:07 +0600

    install modules

--- Backend/Weather Project/script.js
+++ Backend/Weather Project/script.js
@@ -10,7 +10,8 @@ app.get('/',(req,res)=>{
     const urlOfOpenWeatherAPI= "https://api.openweathermap.org/data/2.5/weather?q=New York&appid=091407001ef812a094e745e66dde647b&units=metric"
     https.get(urlOfOpenWeatherAPI,(response)=>{
         console.log(response.statusCode)
-        response.on('data', )
+        console.log(response)
+    //     response.on('data', )
     })
 
 
